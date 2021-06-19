5cc9ee29e02695230bfffed196a126a410ba4181
Qiu Jueqin <jqx1991@gmail.com>
2021-06-19 21:57:53 +0800

    refactor: Update usage examples

--- examples/main.py
+++ examples/main.py
@@ -19,23 +19,22 @@ def _print(config, title):
 default_config_path = './default_config.yaml'
 cfg = Config(default_config_path)
 
-_print(cfg, 'Default Config:')
+_print(cfg, 'Default config:')
 
 
 # ========== Example 2 ==========
-# Load user-specific config from another yaml file
-# and override those attributes in default config.
+# Load user-specific config from another yaml file and override those attributes in default config.
 user_config_path = './user_config.yaml'
-cfg.merge(user_config_path, allow_new_attributes=True)
+cfg.merge(user_config_path)
 
-_print(cfg, 'User-Specific Config:')
+_print(cfg, 'User-specific config:')
 
 
 # ========== Example 3 ==========
 # Use context manager to modify some attributes.
 with cfg.unfreeze():
-    cfg.data.batch_size = 512
-    cfg.data.subsets = ['mini_test']
+    cfg.model.backbone = 'resnet101'
+    cfg.data.batch_size = 1024
 
 _print(cfg, 'Config after on-spot modification:')
 
@@ -43,35 +42,31 @@ _print(cfg, 'Config after on-spot modification:')
 # ========== Example 4 ==========
 # Use context manager to add new attributes.
 with cfg.unfreeze():
-    cfg.exp = Config()
-    cfg.exp.total_epochs = 300
+    cfg.model.head = 'rpn'  # add a leaf attribute ('head' in 'model' node)
+    cfg.exp = Config({'epochs': 500})  # add a new node ('exp') and leaf attribute ('epochs')
 
-_print(cfg, 'Config with new attribute:')
+_print(cfg, 'Config with new attributes and nodes:')
 
 
 # ========== Example 5 ==========
-# Override some attributes from an dict.
-# Note that when setting 'keep_existed_attributes' to False,
-# original attributes in the source config will be removed if
-# they are not in the new config. You can do this if requires
-# to replace some parent field (here the whole cfg.lr_scheduler
-# field)
-lr_scheduler_config = {
-    'lr_scheduler_name': 'gamma_decay',
-    'decay_epochs': 50,
-    'gamma': 0.5
-}
-cfg.lr_scheduler.merge(lr_scheduler_config,
-                       allow_new_attributes=True,
-                       keep_existed_attributes=False)
+# Override some attributes from an dict.  Note that when setting 'keep_existed_attributes' to
+# False, original attributes in the source config will be removed if they are not in the new config.
+# You can do this if requires to replace a node (here the whole cfg.train node) instead of adding
+# attributes to it.
+train_config = {'optimizer': 'Adam', 'lr': 1E-5}
+cfg.train.merge(train_config,
+                allow_new_attributes=True,
+                keep_existed_attributes=False)
 
-_print(cfg, 'Config after replacing cfg.lr_scheduler field:')
+# Attribute 'momentum' is gone since the new 'train' node doesn't contain this attribute
+_print(cfg, 'Config after replacing cfg.train node:')
 
 
 # ========== Example 6 ==========
-# Remove some attributes.
+# Remove some attributes/nodes.
 with cfg.unfreeze():
-    cfg.remove('optimizer')
-    cfg.remove('lr_scheduler')
+    cfg.remove('data')
+    cfg.remove('exp')
+    cfg.model.remove('head')
 
-_print(cfg, 'Config after removing fields:')
+_print(cfg, 'Config after removing attributes and nodes:')
